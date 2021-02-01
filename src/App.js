4f99b0a3c76140b3d03670713b0b1003a27ff9f1
Nick Snyder <nsnyder1992@gmail.com>
2021-02-01 17:10:29 -0500

    update to functinal components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
+import Header from "./components/site/Header";
+import Footer from "./components/site/Footer";
+import Sidebar from "./components/site/Sidebar";
+
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Sidebar />
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
